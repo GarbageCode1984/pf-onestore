d83aa16610f56b4f01147859a246b2a6d6fb8759
GarbageCode <75023330+GarbageCode1984@users.noreply.github.com>
2022-10-18 18:18:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,4 +11,32 @@ $(function () {
         prevArrow: "<button type='button' class='slick-prev'></button>",
         nextArrow: "<button type='button' class='slick-next'></button>",
     });
+
+    $(window).scroll(function () {
+        let navbar = $(this).scrollTop();
+        let $top = $('.top');
+        if (navbar > 0) {
+            $top.addClass('on');
+        } else {
+            $top.removeClass('on');
+        }
+    });
+
+    $('menu').click(function () {});
+
+    /*     let menuBtn = 0;
+
+    if (menuBtn == 0) {
+        $('.menu').click(function () {
+            $('.top').toggleClass('menuDown');
+            menuBtn = 1;
+            console.log(menuBtn);
+        });
+    } else {
+        $('.menu').click(function () {
+            $('.top').toggleClass('menuDown');
+            menuBtn = 0;
+            console.log(menuBtn);
+        });
+    } */
 });

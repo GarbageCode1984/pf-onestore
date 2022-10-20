587e2677e9b2f37bf55490815892b4a15b2dc711
GarbageCode <75023330+GarbageCode1984@users.noreply.github.com>
2022-10-20 09:23:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-    $(".sliderWrap").slick({
+    $('.sliderWrap').slick({
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
@@ -14,25 +14,65 @@ $(function () {
 
     $(window).scroll(function () {
         let navbar = $(this).scrollTop();
-        let $top = $(".top");
+        let $top = $('.top');
         if (navbar > 0) {
-            $top.addClass("on");
+            $top.addClass('on');
         } else {
-            $top.removeClass("on");
+            $top.removeClass('on');
         }
     });
 
     let clickMenuBtn = 0;
-    $(".menu").click(function () {
+    $('.menu').click(function () {
         if (clickMenuBtn == 0) {
-            $(".top").removeClass("menuUp");
-            $(".top").addClass("menuDown");
+            $('.top').removeClass('menuUp');
+            $('.top').addClass('menuDown');
+            $('button').removeClass('menu');
+            $('button').addClass('menuX');
+
+            $('.gnb').show();
+            $('.enb span').show();
+            $('.mobile_p').show();
+            $('.gnbsub li').show();
+
             clickMenuBtn = 1;
         } else {
-            $(".top").removeClass("menuDown");
-            $(".top").addClass("menuUp");
+            $('.top').removeClass('menuDown');
+            $('.top').addClass('menuUp');
+            $('button').removeClass('menuX');
+            $('button').addClass('menu');
+
+            $('.gnb').hide();
+            $('.enb span').hide();
+            $('.mobile_p').hide();
+            $('.gnbsub li').hide();
 
             clickMenuBtn = 0;
+        }
+    });
+
+    $(window).resize(function () {
+        let winW = window.innerWidth;
+        if (winW > 1023) {
+            if ($('.top').has('menuUp')) {
+                // mobile -> pc로 넘어갈때 menuUp이 있으면 헤더부분 보이게
+                $('.top').removeClass('menuUp');
+                $('.gnb').css('display', 'flex');
+                $('.enb span').css('display', 'flex');
+                $('.gnbsub > li').css('display', 'inline-block');
+            }
+            if ($('.top').has('menuDown')) {
+                // mobile -> pc로 넘어갈때 menuDown이 있으면 헤더부분 보이게
+                $('.top').removeClass('menuDown');
+                $('.mobile_p').css('display', 'none');
+                $('.gnbsub > li').css('display', 'inline-block');
+            }
+        } else {
+            //pc -> mobile로 넘어갈때
+            $('button').removeClass('menuX');
+            $('button').addClass('menu');
+            $('.gnb').css('display', 'none');
+            $('.enb span').css('display', 'none');
         }
     });
 });
